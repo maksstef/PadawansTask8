02567832dab9b72d12b36e6f3f4dcc69753a1fe8
mxmstfnk <boss.canta@mail.ru>
2019-02-19 12:17:57 +0300

    2

--- WordsManipulation.cs
+++ WordsManipulation.cs
@@ -7,48 +7,59 @@ namespace PadawansTask8
     {
         public static void RemoveDuplicateWords(ref string text)
         {
-
-            char[] suka = text.ToCharArray();
-            for (int i = 0; i < suka.Length; ++i)
+            if(text == null)
             {
-                if (text[i] == ',' || text[i] == '.' || text[i] == '?' ||
-                    text[i] == '!' || text[i] == ':' || text[i] == ';' || text[i] == '-')
-                {
-                    suka[i] = ' ';
-                }
-                else
-                {
-                    suka[i] = text[i];
-                }
+                throw new ArgumentException();
             }
-            string text2 = new string(suka);
-
-            string[] hh = text2.Split(' ');
-
-            for (int i = 0; i < hh.Length; ++i)
+            else if(text.Length == 0)
             {
-                for (int j = i + 1; j < hh.Length; ++j)
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                char[] suka = text.ToCharArray();
+                for (int i = 0; i < suka.Length; ++i)
                 {
-                    if (hh[i] == hh[j] && hh[i] != " ")
+                    if (text[i] == ',' || text[i] == '.' || text[i] == '?' ||
+                        text[i] == '!' || text[i] == ':' || text[i] == ';' || text[i] == '-')
                     {
-                        hh[j] = " ";
+                        suka[i] = ' ';
+                    }
+                    else
+                    {
+                        suka[i] = text[i];
                     }
                 }
-            }
+                string text2 = new string(suka);
 
-            string final = "";
-            for (int i = 0; i < hh.Length-1; ++i)
-            {
-                if(hh[i] != " ")
+                string[] hh = text2.Split(' ');
+
+                for (int i = 0; i < hh.Length; ++i)
                 {
-                    final += hh[i]+" ";
+                    for (int j = i + 1; j < hh.Length; ++j)
+                    {
+                        if (hh[i] == hh[j] && hh[i] != " ")
+                        {
+                            hh[j] = " ";
+                        }
+                    }
                 }
-                else
+
+                string final = "";
+                for (int i = 0; i < hh.Length - 1; ++i)
                 {
-                    final += hh[i]+"";
+                    if (hh[i] != " ")
+                    {
+                        final += hh[i] + " ";
+                    }
+                    else
+                    {
+                        final += hh[i] + "";
+                    }
                 }
+                text = final;
             }
-            text = final;
+            
         }
     }
 }
